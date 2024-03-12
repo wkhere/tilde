bd9b4728e93dc0d470162ceb19a5d4abfe4d0066
Wojciech Kaczmarek <wojtekk@kofeina.net>
2024-03-12 14:19:28 +0100

    fix windows build

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wkhere/tilde
 
 go 1.18
+
+retract v0.2.0
--- home_windows.go
+++ home_windows.go
@@ -1,5 +1,7 @@
 package tilde
 
+import "os"
+
 func homeFromEnv() (string, error) {
 	if v := os.Getenv("HOME"); v != "" {
 		return v, nil
